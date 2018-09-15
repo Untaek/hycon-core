bf468cf5733ce0b05d4e00f14c837bc59df2ff9f
Untaek <wkblack11@gmail.com>
2018-09-15 18:08:20 +0900

    4

--- hack/src/server.ts
+++ hack/src/server.ts
@@ -2,8 +2,6 @@ import * as Express from "express"
 import { getLogger } from 'log4js'
 import * as morgan from 'morgan'
 import * as bodyparser from 'body-parser'
-import { IHyconWallet } from "./interfaces";
-import * as request from 'request-promise-native'
 import { DB } from "./db";
 import { Wallet } from "../../src/wallet/wallet"
 import axios from 'axios'
@@ -50,7 +48,7 @@ export class Server {
     try {
       return await this.db.singleQuery('SELECT address FROM address WHERE name=?', [nickname])[0]
     } catch (e) {
-      throw e
+      return false
     }
   }
 
@@ -78,7 +76,7 @@ export class Server {
         })
     })
 
-    router.post('/wallet/recover', async (req, res) => {
+    router.post('/wallet/recover', async (req, res) => {    // wallet recover
       const password: string = req.body.password
       const nickname: string = req.body.nickname
       const mnemonic: string = req.body.mnemonic
@@ -100,7 +98,7 @@ export class Server {
       })
     })
 
-    router.get('/wallet/:address', async (req, res) => {
+    router.get('/wallet/:address', async (req, res) => {      // my wallet 
       try {
         const resp = await axios.get(`${this.apiUrl}/wallet/${req.params.address}/balance`)
         const hyc = await axios.get('https://www.okex.com/api/v1/ticker.do?symbol=hyc_btc')
@@ -109,18 +107,19 @@ export class Server {
         const usdPerBTC = btc.data.ticker.last
         const kwrPerUSD = 1100
 
-        const balance = resp.data.status === 404 ? 0 : resp.data
+        const balance = resp.data.status === 404 ? 0 : resp.data.balance
+        console.log(balance)
         res.json({
-          hyconPrice: BTCPerHYC * usdPerBTC * kwrPerUSD,
+          hyconPrice: (BTCPerHYC * usdPerBTC * kwrPerUSD).toFixed(2),
           balance: balance,
-          krw: BTCPerHYC * usdPerBTC * kwrPerUSD * balance
+          krw: (BTCPerHYC * usdPerBTC * kwrPerUSD * balance).toFixed(2)
         })
       } catch (e) {
-        console.log(e)
+        res.json({ error: "fetch error" })
       }
     })
 
-    router.post('/tx', async (req, res) => {
+    router.post('/tx', async (req, res) => {        // send coin
       console.log(req.body.privateKey)
       const privateKey = Wallet.decryptAES(String(req.body.password), Buffer.from(String(":" + req.body.privateKey)))
       console.log(privateKey)
@@ -137,7 +136,7 @@ export class Server {
       })
     })
 
-    router.get('/wallet/:address/txs', async (req, res) => {
+    router.get('/wallet/:address/txs', async (req, res) => {    // get coin
       const address = req.params.address
 
       axios.get(`${this.apiUrl}/wallet/${address}/txs`)
